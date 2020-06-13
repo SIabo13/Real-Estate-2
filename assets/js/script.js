0218d2ff7d7d555af541471ba24f885d3e77b0be
Raul <raul0882@gmail.com>
2020-06-13 12:17:45 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// $('document').ready(function(){
+//     $('.img_overlay').mouseover(function(){
+//         $(this).animate({'background':'white',
+//                     'font-size':'50px',
+//                      'width': "350px",
+//                      "height": '170px'
+//                     });
+//         $(".overlay_btn").css('display','inherit');
+//     })
+// })
